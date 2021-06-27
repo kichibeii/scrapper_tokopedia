3f0458e2a53696adc89e8225da17f431c4384fc9
mukhibillah <mukhib.cascuser303@gmail.com>
2021-06-27 23:39:42 +0700

    add product_name, description and price

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -23,8 +25,8 @@ type Data struct {
 }
 
 func main() {
-
-	datas := scrawlData()
+	datas := straw()
+	// datas := scrawlData()
 
 	writeDataToCSV(datas)
 }
@@ -122,4 +124,83 @@ func getRealIP(fakeIP string) string {
 	productLinks := strings.Split(productLink, "%3F")
 
 	return fmt.Sprintf("%s/%s/%s", domain, store, productLinks[0])
+}
+
+type movie struct {
+	Title string
+	Year  string
+}
+type star struct {
+	Name      string
+	Photo     string
+	JobTitle  string
+	BirthDate string
+	Bio       string
+	TopMovies []movie
+}
+
+func straw() []Data {
+	month := flag.Int("month", 1, "Month to fetch birthdays for")
+	day := flag.Int("day", 1, "Day to fetch birthdays for")
+	flag.Parse()
+	datas := crawl(*month, *day)
+	return datas
+}
+
+func crawl(month int, day int) []Data {
+	allDatas := make([]Data, 0)
+	c := colly.NewCollector(
+		// using async makes you lose the sort order
+		// colly.Async(true)
+		colly.AllowedDomains("www.tokopedia.com", "tokopedia.com"),
+		colly.UserAgent("xy"),
+	)
+
+	infoCollector := c.Clone()
+
+	c.OnHTML(".e1nlzfl3", func(e *colly.HTMLElement) {
+		profileUrl := e.ChildAttr("a", "href")
+		profileUrl = e.Request.AbsoluteURL(profileUrl)
+		infoCollector.Visit(profileUrl)
+	})
+
+	infoCollector.OnHTML("#main-pdp-container", func(e *colly.HTMLElement) {
+		productName := e.ChildText("h1.css-1wtrxts")
+		description := e.ChildText("span.css-168ydy0")
+		Price := e.ChildText("div.price")
+		rating := e.ChildText("#lblPDPDetailProductRatingNumber")
+		nameShop := e.ChildText("a.css-1n8curp > h2")
+
+		var imageLink []string
+		e.ForEach("div.css-1aplawl", func(_ int, kf *colly.HTMLElement) {
+			linkImage := kf.ChildAttr("div.css-19i5z4j > img.success", "src")
+			imageLink = append(imageLink, linkImage)
+		})
+
+		allDatas = append(allDatas, Data{
+			ProductName: productName,
+			Description: description,
+			Price:       Price,
+			Rating:      rating,
+			ImageLink:   imageLink,
+			NameOfStore: nameShop,
+		})
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL.String())
+	})
+
+	infoCollector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting Profile URL: ", r.URL.String())
+	})
+
+	c.Visit("https://tokopedia.com/p/handphone-tablet/handphone")
+
+	js, err := json.MarshalIndent(allDatas, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(js))
+	return allDatas
 }
